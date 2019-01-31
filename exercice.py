882834e28d6b217d198157db3e542782bbef7665
Nicolas <nicolas.van.bastelaer@skynet.be>
2019-01-31 14:44:37 +0100

    renommage

--- exercice.py
+++ exercice.py
@@ -1,29 +1,34 @@
 import random
-fichier=open("B12_liste1.txt","r") ##Lecture des fichiers txt
-liste_B12B=[]
-liste_B12A=fichier.read().splitlines()##conversion du contenu des fichiers en une liste
-liste_B12B.extend(liste_B12A)
-indiceB=0
-while len(liste_B12A) != 0:
-A=random.choice(liste_B12A)
-B=random.choice(liste_B12B)
-if A == B: ##Empêche que les deux mêmes noms soient sélectionnés en même temps
-liste_B12B.remove(A)
-B=random.choice(liste_B12B)
-liste_B12B.append(A)
-if len(liste_B12A)==2: ##Supprime la faible probabilité que les deux derniers noms de la liste soient les mêmes
-    for nom in liste_B12A:
-        if nom == liste_B12B[0]:
-            A=nom
-            B=liste_B12B[1]
-        if nom == liste_B12B[1]: 
-            A=nom
-            B=liste_B12B[0] 
-if A in liste_B12B == False and B in liste_B12A == False:
-    save=B
-    liste_B12B.remove(B)
-    B=random.choice(liste_B12B)
-    liste_B12B.append(save)
-    print(A+" doit offrir un cadeau à "+B) ##Affiche le résultat du tirage au sort
-liste_B12A.remove(A)##Supprime les deux noms tirés au sort de la liste
-liste_B12B.remove(B)
+
+
+fichier = open("B12_liste1.txt","r")  ##Lecture des fichiers txt
+receveurs = []
+donneurs = fichier.read().splitlines()  ##conversion du contenu des fichiers en une liste
+receveurs.extend(donneurs)
+
+while len(donneurs) != 0:
+    donneur = random.choice(donneurs)
+    receveur = random.choice(receveurs)
+    if donneur == receveur: ##Empêche que les deux mêmes noms soient sélectionnés en même temps
+        receveurs.remove(donneur)
+        receveur = random.choice(receveurs)
+        receveurs.append(donneur)
+    if len(donneurs) == 2: ##Supprime la faible probabilité que les deux derniers noms de la liste soient les mêmes
+        for nom in donneurs:
+            if nom == receveurs[0]:
+                donneur = nom
+                receveur = receveurs[1]
+            if nom == receveurs[1]: 
+                donneur = nom
+                receveur = receveurs[0]
+                
+    if donneur in receveurs == False and receveur in donneurs == False:
+        # Evite l'échange mutuel de cadeaux
+        save = receveur
+        receveurs.remove(receveur)
+        receveur = random.choice(receveurs)
+        receveurs.append(save)
+    print(donneur + " doit offrir un cadeau à " + receveur) ##Affiche le résultat du tirage au sort
+    donneurs.remove(donneur)  ##Supprime les deux noms tirés au sort de la liste
+    receveurs.remove(receveur)
+    
