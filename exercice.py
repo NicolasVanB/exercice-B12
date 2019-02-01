3a029b0323c645c1e42f254b95dc28bfba05ab66
Nicolas <nicolas.van.bastelaer@skynet.be>
2019-02-01 14:29:51 +0100

    nouvelle version du programme

--- exercice.py
+++ exercice.py
@@ -1,12 +1,15 @@
 import random
 
+couples_acceptés=[]
 
-fichier = open("B12_liste1.txt","r")  ##Lecture des fichiers txt
-receveurs = []
-donneurs = fichier.read().splitlines()  ##conversion du contenu des fichiers en une liste
-receveurs.extend(donneurs)
+def get_donneurs_receveurs():
+    fichier = open("B12_liste1.txt","r")  ##Lecture des fichiers txt
+    receveurs = []
+    donneurs = fichier.read().splitlines()  ##conversion du contenu des fichiers en une liste
+    receveurs.extend(donneurs)
+    return donneurs, receveurs
 
-while len(donneurs) != 0:
+def candidats_donneurs_receveurs(donneurs, receveurs):
     donneur = random.choice(donneurs)
     receveur = random.choice(receveurs)
     if donneur == receveur: ##Empêche que les deux mêmes noms soient sélectionnés en même temps
@@ -20,15 +23,94 @@ while len(donneurs) != 0:
                 receveur = receveurs[1]
             if nom == receveurs[1]: 
                 donneur = nom
-                receveur = receveurs[0]
-                
-    if donneur in receveurs == False and receveur in donneurs == False:
-        # Evite l'échange mutuel de cadeaux
-        save = receveur
+                receveur = receveurs[0]     
+    if (receveur, donneur) in couples_acceptés:#empêche l'échange mutuel
+        print("Itération déclenchée")
+        sauvegarde = receveur
         receveurs.remove(receveur)
         receveur = random.choice(receveurs)
-        receveurs.append(save)
-    print(donneur + " doit offrir un cadeau à " + receveur) ##Affiche le résultat du tirage au sort
+        receveurs.append(sauvegarde)
+        print("Echange mutuel corrigé")
     donneurs.remove(donneur)  ##Supprime les deux noms tirés au sort de la liste
     receveurs.remove(receveur)
+    couples_acceptés.append((donneur,receveur))
+    return donneur, receveur
+
+
+def trouve_tous_les_donneurs_receveurs(donneurs, receveurs):
+    print()
+    compteur=0
+    couples = []
+    while len(donneurs) != 0:
+        compteur += 1
+        couple = candidats_donneurs_receveurs(donneurs, receveurs)
+        couples.append(couple)
+        donneur, receveur = couple
+        print(donneur + " doit offrir un cadeau à " + receveur) ##Affiche le résultat du tirage au sort
+        if compteur == 100:
+            break
+    return couples
+
+def verifie_regles_couples(couples):
+    liste_echanges_mutuels=[]
+    liste_echanges_solo=[]
+    liste_echanges_doubles=[]
+    echange_solo = False
+    echange_mutuel = False
+    echange_double = False
+    for couple in couples:
+        for candidats in couples:
+            if candidats[0] == couple[1] and candidats[1] == couple[0]:
+                echange_mutuel = True
+                liste_echanges_mutuels.append(candidats)
+            if candidats[0] == candidats[1]:
+                echange_solo = True
+                liste_echanges_solo.append(candidats)
+            if couples.count(candidats)<1:
+                echange_double = True
+                liste_echanges_doubles.append(candidats)
+    print()
+    if echange_mutuel == True:
+        print("erreur: échange mutuel")
+    if echange_solo == True:
+        print("erreur: échange à soi-même")
+    if echange_double == True:
+        print ("erreur: échange double")
+    print("liste des problèmes d'échanges mutuels: ",liste_echanges_mutuels)
+    print("liste des problèmes d'échanges à soi-même: ",liste_echanges_solo)
+    print("liste des problèmes d'échanges doubles: ",liste_echanges_doubles)
+    if liste_echanges_mutuels == [] and liste_echanges_solo == [] and liste_echanges_doubles == []:
+        print("Aucune erreur détectée")
+def cas_reel():
+    donneurs, receveurs = get_donneurs_receveurs()        
+    couples = trouve_tous_les_donneurs_receveurs(donneurs, receveurs)
+    verifie_regles_couples(couples)
+
+def cas_trivial():
+    donneurs = [
+        "A",
+        "B"
+        ]
+    receveurs = [
+        "A",
+        "B"
+        ]
+    couples = trouve_tous_les_donneurs_receveurs(donneurs, receveurs)
+    verifie_regles_couples(couples)
     
+def simple_cas():
+    donneurs = [
+        "A",
+        "B",
+        "C"
+        ]
+    receveurs = [
+        "A",
+        "B",
+        "C"
+        ]
+    couples = trouve_tous_les_donneurs_receveurs(donneurs, receveurs)
+    verifie_regles_couples(couples)
+#cas_trivial()
+simple_cas()
+cas_reel()
